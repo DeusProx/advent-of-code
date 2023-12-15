ab7996227bdc537b9145fc3ecc85822c98aff699
Gordon Lawrenz <gordon@lawrenz.email>
2023-12-16 00:25:59 +0100

    add day-09

---
+++ day-09/src/part2.rs
@@ -0,0 +1,49 @@
+use std::fs;
+
+fn main() {
+    let input: String = fs::read_to_string("./input").expect("Cannot read input file");
+    let result = calc(&input);
+    println!("Result: {:?}", result);
+    assert!(result == (1026, 1584748274));
+}
+
+fn calc(input: &str) -> (i32, i32) {
+    input.lines()
+        .map(|line| {
+            line.split(" ")
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect()
+        })
+        .map(|history| analyse(history))
+        .fold((0,0),|acc, (before, after)| (acc.0 + before, acc.1 + after))
+}
+
+fn analyse(history: Vec<i32>) -> (i32, i32) {
+    match history.iter().any(|val| *val != 0) {
+        true => {
+            let extrapolation = history.as_slice()
+                .windows(2)
+                .map(|window| window.last().unwrap() - window.first().unwrap())
+                .collect::<Vec<i32>>();
+            let (before, after) = analyse(extrapolation.clone());
+            (
+                -before +  history.first().unwrap(),
+                after + history.last().unwrap(),
+            )
+        },
+        false => (0, 0),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::calc;
+
+    #[test]
+    fn test() {
+        let input: String = std::fs::read_to_string("./test").expect("Cannot read input file");
+        let result = calc(&input);
+        assert!(result == (2, 114));
+    }
+}
+

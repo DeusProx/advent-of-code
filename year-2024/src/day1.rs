00b9935de31cb3ae96928f65e4d08f7a4fe3e258
Gordon Lawrenz <gordon@lawrenz.email>
2024-12-02 01:18:35 +0100

    solution for 2024/1

---
+++ year-2024/src/day1.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+
+pub fn day1_part1() -> u32 {
+    // TODO: Solve with macro
+    let input = std::fs::read_to_string("../data/2024/day/1/input").expect("Cannot read input");
+
+    let (mut left, mut right): (Vec<u32>, Vec<u32>) = input.lines()
+        .map(|line| {
+            let n: Vec<u32> = line
+                .split_whitespace()
+                .map(|n| n.parse().ok().unwrap())
+                .collect();
+            (n[0], n[1])
+        })
+        .unzip();
+
+
+    left.sort();
+    right.sort();
+
+    left.iter().zip(right)
+        .map(|(l, r)| l.abs_diff(r))
+        .sum()
+}
+
+pub fn day1_part2() -> u32 {
+    // TODO: Solve with macro
+    let input = std::fs::read_to_string("../data/2024/day/1/input").expect("Cannot read input");
+
+    let (left, right): (Vec<u32>, Vec<u32>) = input.lines()
+        .map(|line| {
+            let n: Vec<u32> = line
+                .split_whitespace()
+                .map(|n| n.parse().ok().unwrap())
+                .collect();
+            (n[0], n[1])
+        })
+        .unzip();
+
+    let right_count = right.iter().fold(
+        HashMap::with_capacity(input.lines().count()),
+        | mut map: HashMap<&u32, u32>, number | {
+            match map.get(number) {
+                Some(value) => map.insert(number, *value + 1),
+                None => map.insert(number, 1),
+            };
+            map
+        }
+    );
+
+    left.iter()
+        .map(|number| number * right_count.get(number).unwrap_or(&0))
+        .sum()
+}
---
+++ year-2024/src/main.rs
@@ -0,0 +1,6 @@
+use aoc_days::{self, day1::{day1_part1, day1_part2}};
+
+fn main() {
+    println!("day  1, part 1: {}", day1_part1());
+    println!("day  1, part 2: {}", day1_part2());
+}

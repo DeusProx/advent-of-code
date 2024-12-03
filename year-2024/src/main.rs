2d53458c364cbbd8dcbf234bf2937824f4b8883e
Gordon Lawrenz <gordon@lawrenz.email>
2024-12-03 23:17:25 +0100

    Solve day 3

--- year-2024/src/main.rs
+++ year-2024/src/main.rs
@@ -1,6 +1,7 @@
 use aoc_days::{
     day1::{day1_part1, day1_part2},
     day2::{day2_part1, day2_part2},
+    day3::{day3_part1, day3_part2},
 };
 
 fn main() {
@@ -16,5 +17,12 @@ fn main() {
     day2_part1();
     println!("  Part 2");
     day2_part2();
+    println!();
+
+    println!("Day 3");
+    println!("  Part 1");
+    day3_part1();
+    println!("  Part 2");
+    day3_part2();
     println!();
 }

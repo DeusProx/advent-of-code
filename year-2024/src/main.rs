d14526acf48066dc719e458c6a7ae4e1e2f63d06
Gordon Lawrenz <gordon@lawrenz.email>
2024-12-12 21:21:21 +0100

    improve file order for coming puzzles

--- year-2024/src/main.rs
+++ year-2024/src/main.rs
@@ -1,13 +1,13 @@
 use aoc_days::{
-    day1::{day1_part1, day1_part2},
-    day2::{day2_part1, day2_part2},
-    day3::{day3_part1, day3_part2},
-    day4::{day4_part1, day4_part2},
-    day5::{day5_part1, day5_part2},
-    day6::{day6_part1, day6_part2},
-    day7::{day7_part1, day7_part2, day7_part2_rayon},
-    day8::{day8_part1, day8_part2},
-    day9::{day9_part1, day9_part2},
+    day01::{day1_part1, day1_part2},
+    day02::{day2_part1, day2_part2},
+    day03::{day3_part1, day3_part2},
+    day04::{day4_part1, day4_part2},
+    day05::{day5_part1, day5_part2},
+    day06::{day6_part1, day6_part2},
+    day07::{day7_part1, day7_part2, day7_part2_rayon},
+    day08::{day8_part1, day8_part2},
+    day09::{day9_part1, day9_part2},
 };
 
 fn main() {
@@ -46,14 +46,12 @@ fn main() {
     day5_part2();
     println!();
 
-
     println!("Day 6");
     println!("  Part 1");
     day6_part1();
     println!("  Part 2");
     day6_part2();
     println!();
-
 
     println!("Day 7");
     println!("  Part 1");
